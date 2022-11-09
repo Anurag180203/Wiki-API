20f0b6e7be87f8d96a1c38077c29ea8ec5beb4f2
Anurag180203 <anurag180203@gmail.com>
2022-11-09 17:10:50 +0530

    add route chaining

--- app.js
+++ app.js
@@ -23,16 +23,43 @@ const articleSchema = new mongoose.Schema({
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function(req, res) {
-    Article.find({}, function(err, foundArticles) {
-        if (!err) {
-            res.send(foundArticles);
-        } else {
-            res.send(err);
-        }
+app.route("/articles")
+    .get(function(req, res) {
+        Article.find({}, function(err, foundArticles) {
+            if (!err) {
+                res.send(foundArticles);
+            } else {
+                res.send(err);
+            }
 
+        })
     })
-});
+    .post(function(req, res) {
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+        newArticle.save(function(err) {
+            if (!err) {
+                res.send("Successfully added a new article.");
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .delete(function(req, res) {
+        Article.deleteMany({}, function(err) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send("Deleted all articles successfully");
+            }
+        });
+    });
+
+
+
+
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
