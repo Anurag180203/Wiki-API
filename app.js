3e4a2e2f80b86f8f918d8f9335042cfafd2849cf
Anurag180203 <anurag180203@gmail.com>
2022-11-09 19:04:24 +0530

    Finish project

--- app.js
+++ app.js
@@ -72,11 +72,47 @@ app.route("/articles/:articleTitle")
                 res.send(foundArticle);
             }
         });
+    })
+    .put(function(req, res) {
+        Article.replaceOne({
+            title: req.params.articleTitle
+        }, {
+            title: req.body.title,
+            content: req.body.content
+        }, {
+            overwrite: true
+        }, function(err) {
+            if (!err) {
+                res.send("Successfully updated article");
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .patch(function(req, res) {
+        Article.updateMany({
+            title: req.params.articleTitle
+        }, {
+            $set: req.body
+        }, function(err) {
+            if (!err) {
+                res.send("Successfully updated article");
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .delete(function(req, res) {
+        Article.deleteOne({
+            title: req.params.articleTitle
+        }, function(err) {
+            if (!err) {
+                res.send("Successfully deleted the corresponding article");
+            } else {
+                res.send(err);
+            }
+        });
     });
-
-
-
-
 
 
 app.listen(3000, function() {
